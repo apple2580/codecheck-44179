986ee5ebddcda3a996fb89aa6e2487954ff7b5ae
tie304304 <tie304304@S1603C12873-T1.tisnt.tis.co.jp>
2018-06-22 22:42:38 +0900

    5

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -5,6 +5,42 @@ public class App {
 		//for (int i = 0, l = args.length; i < l; i++) {
 			//String output = String.format("argv[%s]: %s", i, args[i]);
 
+
+
+		int len = args.length;
+		int len2 = len - 1;
+		int input_num = Integer.parseInt(args[len]);
+		int[] intArray = new int[len2];
+		String[] strArray = new String[len2];
+		int remainder = 1;
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < len2; i++) {
+			intArray[i] = Integer.parseInt(args[i].substring(0,1));
+			remainder = remainder * intArray[i];
+			strArray[i] = args[i].substring(2);
+		}
+
+		if (input_num % remainder == 0) {
+			for (int i = 0; i < len2; i++) {
+				sb.append(strArray[i]);
+			}
+			output = sb.toString();
+		}
+
+
+		/*
+		for (int i = len; i-- > 0;) {
+			if (i == len ) {
+				int num = Integer.parseInt(args[i]);
+			} else {
+
+			}
+		}
+		*/
+
+
+		/*
 		String output = "";
 		String input_1 = args[0].substring(2);
 		String input_2 = args[1].substring(2);
@@ -19,7 +55,7 @@ public class App {
 		} else {
 			output = Integer.toString(input_3);
 		}
-
+*/
 		System.out.println(output);
 	}
 }
