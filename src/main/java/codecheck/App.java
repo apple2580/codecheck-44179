5bd816e05b83e1aae7492e6d54dde9edb12140d1
tie304304 <tie304304@S1603C12873-T1.tisnt.tis.co.jp>
2018-06-22 23:02:53 +0900

    11

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,6 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		//for (int i = 0, l = args.length; i < l; i++) {
-			//String output = String.format("argv[%s]: %s", i, args[i]);
-
 
 		String output = "";
 		int len = args.length;
@@ -39,34 +36,6 @@ public class App {
 			output = Integer.toString(input_num);
 		}
 
-
-		/*
-		for (int i = len; i-- > 0;) {
-			if (i == len ) {
-				int num = Integer.parseInt(args[i]);
-			} else {
-
-			}
-		}
-		*/
-
-
-		/*
-		String output = "";
-		String input_1 = args[0].substring(2);
-		String input_2 = args[1].substring(2);
-		int input_3 = Integer.parseInt(args[2]);
-
-		if (input_3 % 15 == 0) {
-			output = input_1 + input_2;
-		} else if (input_3 % 3 == 0) {
-			output = input_1;
-		} else if (input_3 % 5 == 0) {
-			output = input_2;
-		} else {
-			output = Integer.toString(input_3);
-		}
-*/
 		System.out.println(output);
 	}
 }
